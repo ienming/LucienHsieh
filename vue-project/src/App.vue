5879d43d0c4d91d2d9b6d0d342b05da8a8f39f1d
ienming <hsieh.alan.0726@gmail.com>
2023-11-24 18:35:39 +0800

    update shadow render

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,29 +7,29 @@ import * as PIXI from "pixi.js";
 
 const mouseHoverPj = ref(false)
 
-onMounted(()=>{
-  const app = new PIXI.Application({
-    backgroundAlpha: 0,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    redizeTo: window
-  })
-  document.querySelector("#noiseOverlay").appendChild(app.view)
-  let noiseFilter = new PIXI.NoiseFilter(0.5)
+// onMounted(()=>{
+//   const app = new PIXI.Application({
+//     backgroundAlpha: 0,
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//     redizeTo: window
+//   })
+//   document.querySelector("#noiseOverlay").appendChild(app.view)
+//   let noiseFilter = new PIXI.NoiseFilter(0.5)
   
-  const graphic = new PIXI.Graphics();
-  graphic.beginFill('0xfafafa');
-  graphic.alpha = 0.15;
-  graphic.drawRect(0, 0, app.view.width, app.view.height);
-  graphic.endFill();
-  graphic.filters = [noiseFilter]
+//   const graphic = new PIXI.Graphics();
+//   graphic.beginFill('0xfafafa');
+//   graphic.alpha = 0.15;
+//   graphic.drawRect(0, 0, app.view.width, app.view.height);
+//   graphic.endFill();
+//   graphic.filters = [noiseFilter]
 
-  app.stage.addChild(graphic);
+//   app.stage.addChild(graphic);
 
-  setInterval(() => {
-    noiseFilter.seed = Math.random();
-  }, 50); 
-})
+//   setInterval(() => {
+//     noiseFilter.seed = Math.random();
+//   }, 50); 
+// })
 </script>
 
 <template>
@@ -48,5 +48,5 @@ onMounted(()=>{
     </section>
   </main>
   <Mouse :hover-pj="mouseHoverPj"/>
-  <section id="noiseOverlay" class="fixed top-0 left-0 mix-blend-multiply pointer-events-none"></section>
+  <!-- <section id="noiseOverlay" class="fixed top-0 left-0 mix-blend-multiply pointer-events-none"></section> -->
 </template>
--- vue-project/src/components/Project.vue
+++ vue-project/src/components/Project.vue
@@ -9,7 +9,7 @@ const props = defineProps(['title', 'info'])
 const emits = defineEmits(['hover', 'leave'])
 const coverImg = new URL(`../assets/cover_${props.title}.png`, import.meta.url).href
 
-const [canvasWidth, canvasHeight] = [400, 300]
+const [canvasWidth, canvasHeight] = [800, 600]
 
 const app = new PIXI.Application({
     backgroundAlpha: 0,
@@ -19,49 +19,53 @@ const app = new PIXI.Application({
 });
 
 const container = new PIXI.Container();
-
 container.x = app.screen.width / 2;
 container.y = app.screen.height / 2;
+app.stage.addChild(container);
 
+// Mask
+const maskGraphic = new PIXI.Graphics();
+app.stage.addChild(maskGraphic);
+maskGraphic.x = app.screen.width / 2;
+maskGraphic.y = app.screen.height / 2;
+maskGraphic.lineStyle(0);
+let randomDis = Math.floor(Math.random()*50)
+maskGraphic.beginFill(0x8bc5ff, 0.4);
+maskGraphic.moveTo(-1*180 + Math.sin(0) * randomDis, -1*180 + Math.cos(0) * randomDis);
+maskGraphic.lineTo(180 + Math.cos(0) * randomDis, -1*180 + Math.sin(0) * randomDis);
+maskGraphic.lineTo(180 + Math.sin(0) * randomDis, 180 + Math.cos(0) * randomDis);
+maskGraphic.lineTo(-1*180 + Math.cos(0) * randomDis, 180 + Math.sin(0) * randomDis);
+
+// Noise shadow
+let noiseFilter = new PIXI.NoiseFilter(0.9)
+let blurFilter = new PIXI.BlurFilter(32, 6)
+const shadow = new PIXI.Graphics();
+shadow.x = -90;
+shadow.y = -90;
+shadow.beginFill('0x000000');
+shadow.alpha = 0.5;
+shadow.drawRect(0, 0, 800, 600);
+shadow.endFill();
+shadow.mask = maskGraphic;
+shadow.filters = [blurFilter, noiseFilter]
+container.addChild(shadow);
+
+setInterval(() => {
+    noiseFilter.seed = Math.random();
+  }, 50); 
+
+// Project cover
 const sprite = PIXI.Sprite.from(coverImg)
 // sprite.scale.set(canvasWidth / sprite.width)
 sprite.scale.set(0.4)
 sprite.anchor.set(0.5);
-
+sprite.mask = maskGraphic;
+sprite.eventMode = 'static'
+sprite
+    .on('click', ()=>{window.alert("Go to behance")})
+    .on('pointerover', () => emits('hover'))
+    .on('pointerout', () => emits('leave'))
 container.addChild(sprite);
-
-app.stage.addChild(container);
-
-// let's create a moving shape
-const masking = new PIXI.Graphics();
-
-app.stage.addChild(masking);
-masking.x = app.screen.width / 2;
-masking.y = app.screen.height / 2;
-masking.lineStyle(0);
-
-container.mask = masking;
-let count = 0;
-let randomDis = Math.floor(Math.random()*50)
-masking.beginFill(0x8bc5ff, 0.4);
-masking.moveTo(-1*180 + Math.sin(count) * randomDis, -1*180 + Math.cos(count) * randomDis);
-masking.lineTo(180 + Math.cos(count) * randomDis, -1*180 + Math.sin(count) * randomDis);
-masking.lineTo(180 + Math.sin(count) * randomDis, 180 + Math.cos(count) * randomDis);
-masking.lineTo(-1*180 + Math.cos(count) * randomDis, 180 + Math.sin(count) * randomDis);
-
-// app.ticker.add(() =>
-// {
-//     count += 0.01;
-
-//     masking.clear();
-
-//     masking.beginFill(0x8bc5ff, 0.4);
-//     masking.moveTo(-1*180 + Math.sin(count) * randomDis, -1*180 + Math.cos(count) * randomDis);
-//     masking.lineTo(180 + Math.cos(count) * randomDis, -1*180 + Math.sin(count) * randomDis);
-//     masking.lineTo(180 + Math.sin(count) * randomDis, 180 + Math.cos(count) * randomDis);
-//     masking.lineTo(-1*180 + Math.cos(count) * randomDis, 180 + Math.sin(count) * randomDis);
-// });
-
 
 const canvas = ref(null)
 onMounted(()=>{
@@ -71,9 +75,7 @@ onMounted(()=>{
 
 <template>
     <section class="relative">
-        <!-- <a href=""> -->
-            <div @mouseover="emits('hover')" @mouseleave="emits('leave')" ref="canvas"></div>
-        <!-- </a> -->
+        <div ref="canvas"></div>
         <div class="absolute -bottom-4">
             {{ info.name }}
         </div>
